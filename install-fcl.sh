79defcd330f5741c8bc9231b2ad8a2213f21e549
齐天大胖子 <39656945+PANDATEEN@users.noreply.github.com>
2023-08-18 22:35:07 +0800

    Update install-fcl.sh

--- install-fcl.sh
+++ install-fcl.sh
@@ -14,16 +14,11 @@ if [ ${UBUNTU_VER} != '22.04' ]; then
     exit 1
 fi
 
-sudo apt-get install -y libccd-dev
-
 # FCL - The Flexible Collision Library
 echo ""
-echo "Installing FCL 0.5.0 from source..."
+echo "Installing FCL 0.6.0 from binaray..."
 echo ""
+
 mkdir -p ~/git; cd ~/git
-git clone https://github.com/flexible-collision-library/fcl
-cd fcl; git reset --hard 0.5.0
-mkdir build; cd build
-cmake .. -DFCL_BUILD_TESTS=OFF
-make -j `nproc`
-sudo make install
+wget http://ftp.de.debian.org/debian/pool/main/f/fcl/libfcl0.6_0.6.1-5_amd64.deb
+dpkg -i libfcl0.6_0.6.1-5_amd64.deb
