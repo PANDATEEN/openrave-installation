63a4efdfe97d9466ac0427030418811f50bf35c0
齐天大胖子 <39656945+PANDATEEN@users.noreply.github.com>
2023-08-18 22:29:29 +0800

    Update install-osg.sh

--- install-osg.sh
+++ install-osg.sh
@@ -15,14 +15,14 @@ if [ ${UBUNTU_VER} != '22.04' ]; then
 fi
 
 # OpenSceneGraph
-OSG_COMMIT=1f89e6eb1087add6cd9c743ab07a5bce53b2f480
+OSG_COMMIT=7d290daa738ffc00c6a5fe4c5cc876e49736daee
 echo ""
-echo "Installing OpenSceneGraph 3.4 from source (Commit ${OSG_COMMIT})..."
+echo "Installing OpenSceneGraph 3.6.5 from source (Commit ${OSG_COMMIT})..."
 echo ""
 
 mkdir -p ~/git; cd ~/git
 git clone https://github.com/openscenegraph/OpenSceneGraph.git
-cd OpenSceneGraph; git reset --hard ${OSG_COMMIT}
+cd OpenSceneGraph;# git reset --hard ${OSG_COMMIT}
 mkdir build; cd build
 
 cmake -DDESIRED_QT_VERSION=4 ..
